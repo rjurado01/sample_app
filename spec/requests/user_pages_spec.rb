c1c31895553d5ac38694625a8b3dadb61eb61e6d
drinor <drinor@hotmail.com>
2012-06-18 12:07:17 +0200

    Fin del 9

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -60,10 +60,10 @@ describe "User pages" do
 
 		describe "with valid infromation" do
 			before do
-				fill_in "Name",					with: "Example User"
-				fill_in "Email", 				with: "user@example.com"
-				fill_in "Password",			with:	"foobar"
-				fill_in "Confirmation",	with: "foobar"
+				fill_in "Name",							with: "Example User"
+				fill_in "Email", 						with: "user@example.com"
+				fill_in "Password",					with:	"foobar"
+				fill_in "Confirm Password",	with: "foobar"
 			end
 
 			it "should create a user" do
