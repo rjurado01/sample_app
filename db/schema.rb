54ebb0544c4576610a3ebc41285dc35180f469cc
drinor <drinor@hotmail.com>
2012-06-18 16:14:57 +0200

    Funciona el 10.1

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120617170119) do
+ActiveRecord::Schema.define(:version => 20120618100942) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
