daa44dcc048e9e07badf22d59d6af6d81b421c6a
drinor <drinor@hotmail.com>
2012-06-18 18:24:06 +0200

    Begining 10.3.4

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,10 @@ class User < ActiveRecord::Base
 	validates	:password, presence: true, length: { minimum: 6 }
 	validates	:password_confirmation, presence: true
 
+	def feed
+		Micropost.where("user_id = ?", id)
+	end
+
 	private
 
 		def create_remember_token
