899f53360db5b6ac1f92d4781312b9471a306946
drinor <drinor@hotmail.com>
2012-06-19 13:28:51 +0200

    Begining 11.2.5

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -48,7 +48,7 @@ describe "Authentication" do
 
 	describe "authorization" do
 
-	describe "for non-signed-in users" do
+		describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
 
 			describe "in the Users controller" do
@@ -76,6 +76,18 @@ describe "Authentication" do
         describe "visiting the followers page" do
           before { visit followers_user_path(user) }
           it { should have_selector('title', text: 'Sign in') }
+        end
+      end
+
+			describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { response.should redirect_to(signin_path) }          
         end
       end
 
