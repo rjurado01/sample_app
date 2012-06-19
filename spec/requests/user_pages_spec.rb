b737da5187a449ada4d733590f29a4fc21d3ba5b
drinor <drinor@hotmail.com>
2012-06-19 13:14:16 +0200

    Begining 11.2.4

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -30,7 +30,8 @@ describe "User pages" do
       end
     end
   end
-  describe "signup page" do
+
+	describe "signup page" do
     before { visit signup_path }
 
     it { should have_selector('h1',	text: 'Sign up') }
@@ -144,5 +145,33 @@ describe "User pages" do
         it { should_not have_link('delete', href: user_path(admin)) }
       end
     end
-  end
+	end
+
+	describe "following/followers" do
+    let(:user) { FactoryGirl.create(:user) }
+    let(:other_user) { FactoryGirl.create(:user) }
+    before { user.follow!(other_user) }
+
+    describe "followed users" do
+      before do
+        sign_in user
+        visit following_user_path(user)
+      end
+
+      it { should have_selector('title', text: full_title('Following')) }
+      it { should have_selector('h3', text: 'Following') }
+      it { should have_link(other_user.name, href: user_path(other_user)) }
+    end
+
+    describe "followers" do
+      before do
+        sign_in other_user
+        visit followers_user_path(other_user)
+      end
+
+      it { should have_selector('title', text: full_title('Followers')) }
+      it { should have_selector('h3', text: 'Followers') }
+      it { should have_link(user.name, href: user_path(user)) }
+    end
+	end
 end	
